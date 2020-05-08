83af0a78bd62e87297bbcabfee9c8e8e64a62482
RyoMatsuda@RCO <ryo_matsuda@r.recruit.co.jp>
2020-05-08 17:11:10 +0900

    remove nlp_course image

--- jupyterlab/download_pretrained_models.sh
+++
@@ -1,4 +0,0 @@
-mkdir /pretrained_models
-
-curl https://github.com/singletongue/WikiEntVec/releases/download/20190520/jawiki.all_vectors.100d.txt.bz2 \
-  --output /pretrained_models/jawiki.all_vectors.100d.txt.bz2
