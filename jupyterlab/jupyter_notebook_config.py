111e8a97a94c63a8f11e082cf4036b443584ea6b
RyoMatsuda@RCO <ryo_matsuda@r.recruit.co.jp>
2020-07-05 09:14:56 +0900

    update

--- jupyterlab/jupyter_notebook_config.py
+++ jupyterlab/jupyter_notebook_config.py
@@ -3,4 +3,4 @@ c.NotebookApp.port = 8888
 c.NotebookApp.open_browser = False
 c.NotebookApp.allow_root = True
 c.NotebookApp.password_required = True
-c.NotebookApp.password = u'sha1:50862ccca6f1:198ba99b53fea0e48ec107a71e539aea53ad7ae8'
+c.NotebookApp.password = 'sha1:1cc713dc3e0a:452543c3d9635f8d7a04452415d3b487e09822a3'
